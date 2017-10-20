97bd9376b8c6a24816eb4a24494466d2bc3ca02b
Mason Frith <gedsaro@hotmail.com>
2017-10-19 19:12:24 -0600

    sfdsaf

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -16,54 +16,83 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
 
-            TcpListener server = new TcpListener(IPAddress.Any, 9999);
-            // we set our IP address as server's address, and we also set the port: 9999
+            //create server
+            TcpListener server = new TcpListener(IPAddress.Any, 8000);
 
-            server.Start();  // this will start the server
+            String fromClient;
+            String serverSays;
+            int i = 0;
+            //start server
+            server.Start();
 
-            while (true)   //we wait for a connection
+            Console.WriteLine("server has started");
+
+            TcpClient client = server.AcceptTcpClient();
+            //get streams
+            StreamReader reader = new StreamReader(client.GetStream());
+            StreamWriter writer = new StreamWriter(client.GetStream());
+
+            writer.AutoFlush = true;
+            writer.Flush();
+            //while (true)
+            while((fromClient = reader.ReadLine()) != null)
             {
-                TcpClient client = server.AcceptTcpClient();  //if a connection exists, the server will accept it
 
-                NetworkStream ns = client.GetStream(); //networkstream is used to send/receive messages
-
-                byte[] hello = new byte[100];   //any message must be serialized (converted to byte array)
-                hello = Encoding.Default.GetBytes("hello world");  //conversion string => byte array
-
-                ns.Write(hello, 0, hello.Length);     //sending the message
-
-                while (client.Connected)  //while the client is connected, we look for incoming messages
+                Console.WriteLine("Client: " + fromClient);
+                if(fromClient.Equals("Bye."))
                 {
-                    byte[] msg = new byte[1024];     //the messages arrive as byte array
-                    ns.Read(msg, 0, msg.Length);   //the same networkstream reads the message sent by the client
-                    Console.WriteLine(encoder.GetString(msg).Trim('')); //now , we write the message as string
-                }
-                
-                String p = HttpUtility.UrlDecode(p, System.Text.Encoding.UTF8);
-
-                    //Console.WriteLine("Hello World.");
-                    //Console.ReadLine();
-
-                    ////create server
-                    //TcpListener server = new TcpListener(8000);
-
-                    ////start server
-                    //server.Start();
-
-                    //while(true)
-                    //{
-                    //    TcpClient client = server.AcceptTcpClient();
-
-                    //    //get streams
-                    //    StreamReader reader = new StreamReader(client.GetStream());
-                    //    StreamWriter writer = new StreamWriter(client.GetStream());
-
-                    //    //read and write
-                    //    writer.WriteLine("ECHO>>" + reader.ReadLine());
-                    //    writer.WriteLine("Bye");
-                    //    writer.Flush();
+                    Console.WriteLine("Reach true of fromClient is Bye.");
+                    writer.WriteLine("Bye.");
+                    writer.Flush();
+                    break;
                 }
 
+                serverSays = "Hello I am server " + i;
+                writer.WriteLine(serverSays);
+                writer.Flush();
+                Console.WriteLine("Me: " + serverSays);
+
+                //fromClient = reader.ReadLine();
+                //Console.WriteLine("Client: " + fromClient);
+                //serverSays = "This is line #" + i;
+                //writer.WriteLine(serverSays);
+                //Console.WriteLine("Me: " + serverSays);
+
+                i++;
+            }
         }
     }
 }
+
+
+
+//TcpListener server = new TcpListener(IPAddress.Any, 9999);
+////TcpListener server = new TcpListener(8000);
+//// we set our IP address as server's address, and we also set the port: 9999
+
+//server.Start();  // this will start the server
+
+//while (true)   //we wait for a connection
+//{
+//    TcpClient client = server.AcceptTcpClient();  //if a connection exists, the server will accept it
+
+//    NetworkStream ns = client.GetStream(); //networkstream is used to send/receive messages
+
+//    byte[] hello = new byte[100];   //any message must be serialized (converted to byte array)
+//    hello = Encoding.Default.GetBytes("hello world");  //conversion string => byte array
+
+//    ns.Write(hello, 0, hello.Length);     //sending the message
+
+//    while (client.Connected)  //while the client is connected, we look for incoming messages
+//    {
+//        byte[] msg = new byte[1024];     //the messages arrive as byte array
+//        ns.Read(msg, 0, msg.Length);   //the same networkstream reads the message sent by the client
+//        Console.WriteLine(encoder.GetString(msg).Trim('')); //now , we write the message as string
+//    }
+
+//    String p = "test";
+
+//    p = HttpUtility.UrlDecode(p, System.Text.Encoding.UTF8);
+
+
+//  }
