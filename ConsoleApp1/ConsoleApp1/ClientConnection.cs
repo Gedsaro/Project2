76f021402c03b746e83aa2cd0c1242ea2893d597
Mason Frith <gedsaro@hotmail.com>
2017-10-20 16:09:19 -0600

    Revamping to connect with multiple clients
    -Removed old one to one turn passing chat.
    -created new file, clientconnection, used for each connected client.
    -Server now waits in loop for a client to connect then makes a new ClientConnection object to handle the connection, it then goes back to waiting for a new connection as soon as possible.
    -ClientConnection, takes in the newly conneccted clientsocket and makes and starts a new thead to handle this connection, currently just prints out successful start, waits a bit, then prints a done.

---
+++ ConsoleApp1/ConsoleApp1/ClientConnection.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class ClientConnection
+    {
+        TcpClient clientSocket;
+        // String clientName; //not used
+
+        /**
+        * constructor for new client connection handling
+        * */
+        public ClientConnection(TcpClient tcpClient)
+        {
+            this.clientSocket = tcpClient;
+            //this. clientName = name; //not used
+            Thread clientThread = new Thread(clientConnectionMain);
+            clientThread.Start();
+
+        }
+
+        /**
+         * This is where the server handles each client connection
+         * */
+        private void clientConnectionMain()
+        {
+            Console.WriteLine("New Client connection thread started"); //testing
+            Thread.Sleep(1500);
+            Console.WriteLine("Done sleeping, nothing more to do now");
+
+        }
+
+
+    }
+}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -19,53 +19,33 @@ namespace ConsoleApp1
 
             //create server
             TcpListener server = new TcpListener(IPAddress.Any, 8000);
+            TcpClient clientSocket;
 
             String fromClient;
             String serverSays;
             int i = 0;
             //start server
+            Console.WriteLine(">> Starting Server.");
             server.Start();
 
-            Console.WriteLine("server has started");
-
-            TcpClient client = server.AcceptTcpClient();
-            //get streams
-            StreamReader reader = new StreamReader(client.GetStream());
-            StreamWriter writer = new StreamWriter(client.GetStream());
-
-            writer.AutoFlush = true;
-            writer.Flush();
-            //while (true)
-            while((fromClient = reader.ReadLine()) != null)
+            //start the server loop
+            while (true)
             {
-
-                Console.WriteLine("Client: " + fromClient);
-                if(fromClient.Equals("Bye."))
-                {
-                    Console.WriteLine("Reach true of fromClient is Bye.");
-                    writer.WriteLine("Bye.");
-                    writer.Flush();
-                    break;
-                }
-
-                serverSays = "Hello I am server " + i;
-                writer.WriteLine(serverSays);
-                writer.Flush();
-                Console.WriteLine("Me: " + serverSays);
-
-                //fromClient = reader.ReadLine();
-                //Console.WriteLine("Client: " + fromClient);
-                //serverSays = "This is line #" + i;
-                //writer.WriteLine(serverSays);
-                //Console.WriteLine("Me: " + serverSays);
-
-                i++;
+                Console.WriteLine(">> Waiting for a client to connect.");
+                clientSocket = server.AcceptTcpClient(); // wait for a new client to connect
+                ClientConnection newClient = new ClientConnection(clientSocket); //client connected
+                Console.WriteLine(">> Server Finished creating new client handler");
             }
+
+
+
         }
     }
 }
 
-
+/**
+ * This batch of commented code is from the interweb, so far did not really use much from it
+ * /
 
 //TcpListener server = new TcpListener(IPAddress.Any, 9999);
 ////TcpListener server = new TcpListener(8000);
@@ -97,3 +77,36 @@ namespace ConsoleApp1
 
 
 //  }
+
+
+
+/**
+* the following is my first working one on one, take turns passing messages back and forth
+**/
+
+////get streams
+//StreamReader reader = new StreamReader(client.GetStream());
+//StreamWriter writer = new StreamWriter(client.GetStream());
+
+//writer.AutoFlush = true;
+//writer.Flush();
+////while (true)
+//while((fromClient = reader.ReadLine()) != null)
+//{
+
+//    Console.WriteLine("Client: " + fromClient);
+//    if(fromClient.Equals("Bye."))
+//    {
+//        Console.WriteLine("Reach true of fromClient is Bye.");
+//        writer.WriteLine("Bye.");
+//        writer.Flush();
+//        break;
+//    }
+
+//    serverSays = "Hello I am server " + i;
+//    writer.WriteLine(serverSays);
+//    writer.Flush();
+//    Console.WriteLine("Me: " + serverSays);
+
+//    i++;
+//}
